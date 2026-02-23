b91b7336f9809975fdda4e4515d97aae07b76396
Hugol4ka <hugolaca12300@gmail.com>
2026-02-23 22:49:46 +0100

    Add file 1-last_digit.c, display last digit whit 3 conditions

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	/*Add variable last_digit*/
+	last_digit = n % 10;
+
+	printf("Last digit of %d is %d ", n, last_digit);
+
+	if (last_digit > 5)
+	{
+		printf("is greater than 5");
+	}
+	else if (last_digit == 0)
+	{
+		printf("and not 0");
+	}
+	else
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+	return (0);
+}
