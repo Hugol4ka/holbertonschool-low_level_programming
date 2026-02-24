3ce5535d46b9af6f5f1c34ffbdadf6726bf34207
Hugol4ka <hugolaca12300@gmail.com>
2026-02-24 16:57:30 +0100

    Add 0-putchar.c to print the string "putchar" followed by a newline

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main - Entry Point
+*
+* Return: Always 0
+*/
+	int main(void)
+	{
+		int i;
+		char str[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+		for (i = 0; i < 8; i++)
+			{
+			putchar(str[i]);
+			}
+		putchar('\n');
+		return (0);
+	}
