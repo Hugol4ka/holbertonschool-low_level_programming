10d698ace5b4cacf4ffbbb048139362bff4797d1
Hugol4ka <hugolaca12300@gmail.com>
2026-02-27 15:13:57 +0100

    Add file 7-print_diagonal.c and add void print_diagonal in the header.

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int s;
+	int e;
+
+	if (n < 0)
+	{
+	_putchar('\n');
+	}
+		else
+			for (e = 0; e < n; e++)
+			{
+				for (s = 0; s < e; s++)
+				{
+				_putchar(' ');
+				}
+				_putchar('\\');
+				_putchar('\n');
+			}
+}
