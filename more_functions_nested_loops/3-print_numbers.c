bd71c7ec36104cf40037858ec0faa5674cc2b378
Hugol4ka <hugolaca12300@gmail.com>
2026-02-27 11:18:11 +0100

    test fix error stdout

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -10,9 +10,9 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar (i + '0');
+		_putchar (i + '0'); /*eviter affiche table ascii*/
 	}
 	_putchar ('\n');
 }
