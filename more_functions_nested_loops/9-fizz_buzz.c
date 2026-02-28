436ec58f2188361a2223c492302cb68f22a3746a
Hugol4ka <hugolaca12300@gmail.com>
2026-02-28 14:23:41 +0100

    Add file 9-fizz_buzz.c and test whit gcc Test.

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100, followed by a new line
+ * For multiples of three, prints Fizz instead of the number
+ * For multiples of five, prints Buzz
+ * For multiples of both three and five, prints FizzBuzz
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+		printf("FizzBuzz");
+		}
+			else if (i % 3 == 0)
+			{
+			printf("Fizz");
+			}
+				else if (i % 5 == 0)
+				{
+				printf("Buzz");
+				}
+					else
+					{
+					printf("%d", i);
+					}
+					if (i < 100)
+					{
+					printf(" ");
+					}
+	}
+		printf("\n");
+}
