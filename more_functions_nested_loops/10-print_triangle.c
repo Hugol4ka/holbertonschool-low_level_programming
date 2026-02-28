25db331989ffede7f846b0bb9bf77756f5103e0a
Hugol4ka <hugolaca12300@gmail.com>
2026-02-28 15:53:52 +0100

    Add files 10-print_triangles.c ; 10-main.c and _putchar.c for compil test.

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include"main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+int l, s, d;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (l = 1; l <= size; l++)
+		{
+		for (s = 0; s < (size - l); s++)
+			{
+				_putchar(' ');
+			}
+		for (d = 0; d < l; d++)
+			{
+				_putchar('#');
+			}
+				_putchar('\n');
+		}
+	}
+}
