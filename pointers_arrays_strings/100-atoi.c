1abb16a630a8831a39deb53cf928c3b04aa26496
Hugol4ka <hugolaca12300@gmail.com>
2026-03-03 17:33:48 +0100

    Fix variable name typo and add missing newline at end of file in _atoi function

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -14,8 +14,28 @@ int _atoi(char *s)
 {
 int i = 0;
 int result = 0;
-int signe = 1;
+int sign = 1;
+int found = 0;
 
-	while (s[i] != '\0')
-
+while (s[i] != '\0')
+{
+	if (found == 0 && s[i] == '-')
+	{
+		sign *= -1;
+	}
+	else if (s[i] >= '0' && s[i] <= '9')
+	{
+		found = 1;
+		result = result * 10 - (s[i] - '0');
+	}
+	else if (found == 1)
+	{
+		break;
+	}
+	i++;
+}
+if (sign == 1)
+	return (-result);
+else
+	return (result);
 }
