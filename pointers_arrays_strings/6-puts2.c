4358b72bde8aee655ab2462bb8f186f897b531e8
Hugol4ka <hugolaca12300@gmail.com>
2026-03-03 14:12:51 +0100

    Add code in file 6-puts2.c and 6-main.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * puts2 - imprime un caractère sur deux d'une chaîne,
+ * en commençant par le premier.
+ * @str: la chaîne de caractères à traiter.
+ *
+ * Return: void.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	i++;
+	}
+	_putchar('\n');
+}
