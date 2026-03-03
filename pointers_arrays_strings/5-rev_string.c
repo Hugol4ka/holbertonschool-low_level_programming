218715ba9a2e87f6054a7b93ff720e4fcb851430
Hugol4ka <hugolaca12300@gmail.com>
2026-03-03 11:48:00 +0100

    Add file 5-rev_string.c and 5-main.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include"main.h"
+/**
+ * rev_string - inverse une chaîne de caractères
+ * @s: pointeur vers la chaîne de caractères à modifier
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+
+{
+	int i = 0;
+	int id = 0;
+	char temp;
+
+	while (s[id] != '\0')
+	{
+	id++;
+	}
+	id--;
+	while (i < id)
+	{
+	temp = s[i];
+	s[i] = s[id];
+	s[id] = temp;
+	i++;
+	id--;
+	}
+
+}
+
+
