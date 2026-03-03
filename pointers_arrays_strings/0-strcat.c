27e44480de07d97e2fad46e542408f5aff7bf469
Hugol4ka <hugolaca12300@gmail.com>
2026-03-04 00:30:25 +0100

    Implement _strcat function and update header file; add VSCode settings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string to be appended to dest
+ *
+ * Return: A pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+}
