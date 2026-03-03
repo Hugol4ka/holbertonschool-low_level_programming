ecbe022cd7be17bc56ea5e0e67e031fe993a1357
Hugol4ka <hugolaca12300@gmail.com>
2026-03-03 16:55:15 +0100

    Add _atoi function and corresponding test cases in 100-main.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * _atoi - convertit une chaîne de caractères en entier.
+ * @s: la chaîne à convertir.
+ *
+ * Description: prend en compte tous les signes - et + précédant
+ * le nombre. Si aucun nombre n'est présent, retourne 0.
+ *
+ * Return: l'entier converti depuis la chaîne.
+ */
+
+
+int _atoi(char *s)
+{
+int i = 0;
+int result = 0;
+int signe = 1;
+
+	while (s[i] != '\0')
+
+}
