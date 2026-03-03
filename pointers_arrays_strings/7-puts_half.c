94e50730b76c307528cc221a1ff3eea2a88447cf
Hugol4ka <hugolaca12300@gmail.com>
2026-03-03 15:37:11 +0100

    Add files 7-puts_half.c and 7-main.c  and add void puts_half in main.h

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,5 +7,19 @@
 
 void puts_half(char *str)
 {
+	int i = 0;
+	int len = 0;
 
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	i = len - (len / 2); /*soustraction car nombre impairs*/
+
+		while (str[i] != '\0')
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	_putchar('\n');
 }
