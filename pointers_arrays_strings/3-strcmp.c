0e43621aaf5868be774392030a4e3016824c6678
Hugol4ka <hugolaca12300@gmail.com>
2026-03-04 11:23:52 +0100

    Implement _strcmp function for string comparison and update header file

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: The first string to compare
+ * @s2: The second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
