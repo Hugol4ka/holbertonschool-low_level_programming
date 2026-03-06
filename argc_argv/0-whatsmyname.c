a5d4a8dd9b62ef2ead9853854e756fab0a253ed2
Hugol4ka <hugolaca12300@gmail.com>
2026-03-06 12:04:47 +0100

    Add implementation of the whatsmyname program and create binary executable

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,1 +1,16 @@
 #include<stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("argv = %s\n", argv[0]);
+	return (0);
+}
