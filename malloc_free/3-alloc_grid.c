6a733eef0913541746e69651bd866461a4e919a2
Hugol4ka <hugolaca12300@gmail.com>
2026-03-09 22:44:16 +0100

    Add alloc_grid function for creating a 2D array and print_grid function for displaying it

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include"main.h"
+#include<stdlib.h>
+
+/**
+ * alloc_grid - creates a 2D array of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: pointer to 2D array, or NULL
+ */
+
+**alloc_grid(int width, int height)
+
+{
+	if (width == NULL)
+	{
+		width = "";
+	}
+	if (height == NULL)
+	{
+		height = "";
+	}
+
+}
