09aea8e783cd72efcd79513da9750e6ce91da0e0
Hugol4ka <hugolaca12300@gmail.com>
2026-03-09 23:47:33 +0100

    Fix alloc_grid function to handle memory allocation errors and initialize grid values

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,16 +9,44 @@
  * Return: pointer to 2D array, or NULL
  */
 
-**alloc_grid(int width, int height)
+int **alloc_grid(int width, int height)
 
 {
-	if (width == NULL)
-	{
-		width = "";
-	}
-	if (height == NULL)
-	{
-		height = "";
-	}
+	int **grid;
+	int i = 0;
+	int j = 0;
 
-}
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+		grid = malloc(sizeof(int *) * height);
+		if (grid == NULL) /*verification des adresses*/
+		{
+			return (NULL);
+		}
+		while (i < height)
+		{
+		grid[i] = malloc(sizeof(int) * width);
+			if (grid[i] == NULL)
+			{
+				int k = 0;
+
+				while (k < i) /*Debut du bloc de nettoyage*/
+				{
+				free(grid[k]);
+				k++;
+				}
+				free(grid);
+				return (NULL); /*fin du bloc de nettoyage*/
+			}
+			j = 0;
+			while (j < width)
+			{
+				grid[i][j] = 0;
+				j++;
+			}
+		i++;
+		}
+		return (grid);
+	}
