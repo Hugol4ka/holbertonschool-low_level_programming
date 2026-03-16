0b27c8a33f6041266a964f2615c18113cec05e5d
Hugol4ka <hugolaca12300@gmail.com>
2026-03-16 14:57:44 +0100

    Update array_iterator function documentation for clarity

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,10 +2,12 @@
 #include<stdlib.h>
 
 /**
- * array_iterator - ...
- * @array: ...
- * @size: ...
- * @action: ...
+ * array_iterator - Exécute une fonction sur chaque élément d'un tableau.
+ * @array: Pointeur vers le tableau d'entiers.
+ * @size: Taille du tableau.
+ * @action: Pointeur vers la fonction à utiliser pour chaque élément.
+ *
+ * Return: Rien.
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
