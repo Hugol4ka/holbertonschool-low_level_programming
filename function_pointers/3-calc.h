2930f43dc64633094cb8490f06ef7fcdf04e8dd6
Hugol4ka <hugolaca12300@gmail.com>
2026-03-16 16:27:25 +0100

    Add calculator operations and function mapping

---
+++ function_pointers/3-calc.h
@@ -0,0 +1,28 @@
+#ifndef CALC_H
+#define CALC_H
+
+/**
+ * struct op - Struct op
+ *
+ * @op: opérateur
+ * @f: la fonction associé
+ *
+ * Description: Structure qui associe un opérateur à sa fonction.
+ */
+typedef struct op
+{
+char *op;
+int (*f)(int a, int b);
+} op_t;
+
+/* Prototype pour operations */
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/* Prototype pour menu */
+int (*get_op_func(char *s))(int, int);
+
+#endif
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include"3-calc.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+* op_add - Calcule la somme de deux entiers
+* @a: le premier entier
+* @b: le second entier
+*
+* Return: le resultat de a + b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - Calcule la soustraction de deux entiers
+* @a: le premier entier
+* @b: le second entier
+*
+* Return: le resultat de a - b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - Calcule la multiplication de deux entiers
+* @a: le premier entier
+* @b: le second entier
+*
+* Return: le resultat de a * b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - Calcule la division de deux entiers
+* @a: le premier entier
+* @b: le second entier
+*
+* Return: le resultat de a / b
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - Calcule le modulo de deux entiers
+* @a: le premier entier
+* @b: le second entier
+*
+* Return: le resultat de a % b
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
