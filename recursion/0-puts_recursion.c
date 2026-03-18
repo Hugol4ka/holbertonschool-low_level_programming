0fe7d67429130b773b3ec6872e975e60b43211f7
Hugol4ka <hugolaca12300@gmail.com>
2026-03-18 10:14:14 +0100

    Refactor _puts_recursion function for improved structure and readability

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include"main.h"
-#include<stdio.h>
+#include <unistd.h>
 
 /**
  * _puts_recursion - prints a string, followed by a new line
@@ -16,5 +16,7 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 	else
-	_putchar('\n');
+	{
+		_putchar('\n');
+	}
 }
