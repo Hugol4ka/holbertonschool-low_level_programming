b8ad1cb7c24f3904cd609319ca980363c71b5833
Hugol4ka <hugolaca12300@gmail.com>
2026-03-20 13:46:28 +0100

    First push

---
+++ PROJET_DOFUS/src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "../include/types.h"
+
+/**
+ * main - Point d'entrée pour tester l'inclusion des types
+ *
+ * Return: Toujours 0 (Succès)
+ */
+
+int main(void)
+{
+	printf("Structures Dofus chargées avec succès !\n");
+	return (0);
+}
