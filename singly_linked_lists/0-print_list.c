0f548290108e0721f7f71649a4cc82be4d75a9dc
Hugol4ka <hugolaca12300@gmail.com>
2026-03-26 09:27:28 +0100

    Refactor print_list function to simplify output and remove unused helper functions

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,70 +1,29 @@
 #include"lists.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_num - prints an unsigned int using _putchar
- * @n: the number to print
- */
-void print_num(unsigned int n)
-{
-	if (n / 10)
-	print_num(n / 10);
-	_putchar((n % 10) + '0');
-}
+#include <stdio.h>
 
 /**
  * print_list - prints all the elements of a list_t list
- * @h: pointer to the start of the list
+ * @h: pointer to the head of the list
+ *
  * Return: the number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
-	unsigned int i;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			_putchar('[');
-			_putchar('0');
-			_putchar(']');
-			_putchar(' ');
-			_putchar('(');
-			_putchar('n');
-			_putchar('i');
-			_putchar('l');
-			_putchar(')');
-			_putchar('\n');
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-				_putchar(h->str[i]);
-				_putchar('[');
-				print_num(h->len);
-				_putchar(']');
-				_putchar(' ');
-			for (i = 0; h->str[i] != '\0'; i++)
-			{
-				_putchar(h->str[i]);
-			}
-				_putchar('\n');
+			printf("[%u] %s\n", h->len, h->str);
 		}
-		nodes++;
+		i++;
 		h = h->next;
 	}
-	return (nodes);
+	return (i);
 }
