f6a680ef70a8a7ddfdc817778623cb5f63c7c0f0
Hugol4ka <hugolaca12300@gmail.com>
2026-03-31 21:35:05 +0200

    Enhance documentation for hash_table_get function

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,6 +1,14 @@
 #include"hash_tables.h"
 #include <string.h>
 
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: A pointer to the hash table.
+ * @key: The key to look for.
+ *
+ * Return: The value associated with the element,
+ * or NULL if key couldn't be found.
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
