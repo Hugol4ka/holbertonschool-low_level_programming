d773c65b0ba5af0b73b89509da97f7453522d3e9
Hugol4ka <hugolaca12300@gmail.com>
2026-04-10 11:17:05 +0200

    Refactor error handling in file copy program and remove unused source text file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,56 +18,36 @@ int main(int ac, char **av)
 
 	/* 1. Vérification du nombre d'arguments */
 	if (ac != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-	}
-
 	/* 2. Ouverture de la source */
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
-	}
-
 	/* 3. Ouverture/Création de la destination (0664 = rw-rw-r--) */
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
-	}
-
 	/* 4. Boucle de lecture et d'écriture */
 	while ((n_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		n_wrote = write(fd_to, buffer, n_read);
 		if (n_wrote == -1 || n_wrote != n_read)
-		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
-		}
 	}
-
-	/* Vérification si la boucle s'est arrêtée à cause d'une erreur de lecture */
+	/* Vérif si la boucle s'est arrêtée à cause d'une erreur de lecture */
 	if (n_read == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
-	}
-
 	/* 5. Fermeture des File Descriptors */
 	if (close(fd_from) == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
-	}
 	if (close(fd_to) == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
-	}
-
 	return (0);
 }
